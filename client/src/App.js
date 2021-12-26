991541fb67c299baf8844a9fdf01d1e9c503b84b
adrianmalecki <adrian.malecki99@o2.pl>
2021-12-26 23:08:43 +0100

    added metamask connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import Web3 from "web3";
 import { Link, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import Register from './Register.js';
@@ -8,12 +9,55 @@ import About from './About.js';
 import Patient from './Patient.js';
 import Record from './Record.js';
 import './App.css';
+
+
  
 class App extends Component {
+
+  async componentWillMount() {
+    // Detect Metamask
+    const metamaskInstalled = typeof window.web3 !== 'undefined'
+    this.setState({ metamaskInstalled })
+    if(metamaskInstalled) {
+      await this.loadWeb3()
+      await this.loadBlockchainData()
+    }
+  }
+
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+    }
+  }
+
+
   render() {
     return (
        <Router>
            <div className="App">
+             {this.state.account}
+             
             <ul className="App-header">
               <li>
                 <Link to="/register">Register</Link>
